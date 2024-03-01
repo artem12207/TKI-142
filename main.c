af09314d4344380575e5149b08bdcacb7f33d81f
artem12207 <144312233+artem12207@users.noreply.github.com>
2024-03-01 15:40:15 +0300

    main.c

--- main.c
+++ main.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
 #include <math.h>
-double getA(double x, double y)
-{
-    return (2* cos(x-(3.14/6))) / (0.5 + (pow(sin(y), 2)));
-}
-double getB(double z)
-{
-    return 1+((pow(z , 2)) / (3 + pow(z , 2)) / 5);
-}
+
+/**
+* @brief Возвращает результат функции a
+* @param Параметр "x" для функуии a
+* @param Параметр "y" для функуии a
+* @param Параметр "z" для функуии a
+**/
+double afunc(double x, double y, double z);
+
+/*
+* @brief Возвращает результат функции a
+* @param Параметр "x" для функуии a
+* @param Параметр "y" для функуии a
+* @param Параметр "z" для функуии a
+**/
+double bfunc(double x, double y, double z);
+
+/*
+* @brief Точка входа в программу
+* @return Возвращает 0, если программа работает корректно, иначе не 0
+**/
 int main()
 {
-    const double x = 1.426;
-    const double y = 1.22;
-    const double z = 3.5;
-    double a = getA(x, y);
-    double b = getB(z);
-    printf("%s%lf\n", "a = ", a);
-    printf("%s%lf", "b = ", b);
+    const double x = 0.78;
+    const double y = 1.24;
+    const double z = 0.5;
+    double a = afunc(x, y, z);
+    double b = bfunc(x, y, z);
+
+    printf("a = %lf\n", a);
+    printf("b = %lf\n", b);
+
     return 0;
+}
+
+double afunc(double x, double y, double z)
+{
+    return (2 * pow(z,x)) / (sqrt((pow(y, x) * pow(cos(x + y), 2))) - 3 * z);
+}
+
+double bfunc(double x, double y, double z)
+{
+    return x * exp(sqrt(z)) * cos((pow(x, 2)) / (y * z));
 }
